f34a4c2a56d3422c2c1df77fce9a1a688741038d
MatejKastak <matej.kastak@gmail.com>
2021-05-28 19:38:17 +0200

    utils: Add method to create empty Anki collection

--- orglearn/utils.py
+++ orglearn/utils.py
@@ -3,7 +3,10 @@ import logging
 import os
 import pathlib
 import sys
+import tempfile
 import typing
+
+from anki.collection import Collection
 
 
 @contextlib.contextmanager
@@ -14,6 +17,28 @@ def Workdir(workdir: typing.Union[str, pathlib.Path]) -> typing.Iterator[None]:
     yield
 
     os.chdir(old_dir)
+
+
+@contextlib.contextmanager
+def create_empty_anki_collection() -> typing.Iterator[Collection]:
+    """ContextManager used to create an empty Anki Collection.
+
+    This collection is used for importing and exporting decks. For some reason
+    creating a collection also changes the current working directory. Therefore
+    it is necessary to call `close` as a cleanup."""
+
+    (fd, path) = tempfile.mkstemp(suffix=".anki2", prefix="orglearn")
+    try:
+        os.close(fd)
+        os.unlink(path)
+    except OSError:
+        pass
+
+    col = Collection(path)
+
+    yield col
+
+    col.close(downgrade=False)
 
 
 def setup_loggging(log_level: int) -> None:
